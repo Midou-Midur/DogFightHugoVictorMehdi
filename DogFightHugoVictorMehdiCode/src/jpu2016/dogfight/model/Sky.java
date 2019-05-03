46a3598c2239873558732ec1671e79037e03b5b6
Mehdi <me.8dream@gmail.com>
2019-05-03 13:46:37 +0200

    je pense avoir fini d'implémenter (hormi une méthode)

--- DogFightHugoVictorMehdiCode/src/jpu2016/dogfight/model/Sky.java
+++ DogFightHugoVictorMehdiCode/src/jpu2016/dogfight/model/Sky.java
@@ -1,12 +1,17 @@
 package jpu2016.dogfight.model;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 public class Sky implements IArea {
 	private Dimension dimension;
 
 	public Sky(Dimension dimension) {
-		
+		this.dimension = dimension;
 	}
 
 	@Override
@@ -17,8 +22,18 @@ public class Sky implements IArea {
 	@Override
 	public Image getImage() {
 
-		return null;
+		ArrayList<Image> Images;
+		Images = new ArrayList<Image>();
+		Image sky = null;
+		try {
 
+			sky = ImageIO.read(new File("sky.jpg"));
+			Images.add(sky);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sky;
+		
 	}
 
 }
